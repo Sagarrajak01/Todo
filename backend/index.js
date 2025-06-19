eaabdce8c4060d5172c70bcf476332c69ffb2516
Sagar Rajak <65167758+Sagarrajak01@users.noreply.github.com>
2025-06-19 10:40:09 +0530

    done

--- backend/index.js
+++ backend/index.js
@@ -3,13 +3,16 @@ const { createTodo, updateTodo } = require('./types');
 const { Todo } = require('./db');
 const app = express();
 const port = 3000;
+const cors = require("cors")
 
 app.use(express.json());
+app.use(cors({}))
 
 app.post('/todos', async (req, res) => {
     const createPayload = req.body;
     const parsedPayload = createTodo.safeParse(createPayload);
-    if (!parsedPayload.success) {
+
+    if (!parsedPayload.success) { 
         res.status(411).json({ message: "You sent the wrong input" });
         return;
     }
@@ -23,25 +26,5 @@ app.post('/todos', async (req, res) => {
     res.json({ message: "Todo Created" });
 });
 
-app.get('/todos', async (req, res) => {
-    const todos = await Todo.find({});
-    res.json({ todos });
-});
-
-app.put('/completed', async (req, res) => {
-    const updatePayload = req.body;
-    const parsedPayload = updateTodo.safeParse(updatePayload);
-    if (!parsedPayload.success) {
-        res.status(411).json({ message: "Invalid Input" });
-        return;
-    }
-
-    await Todo.updateOne(
-        { _id: req.body.id },
-        { $set: { completed: true } }
-    );
-
-    res.json({ message: "Todo marked as completed" });
-});
 
 app.listen(port)
